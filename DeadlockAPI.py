5abc32689f869eac6485ce2dc4af9a32316c5a6c
HammsN <fahadmahmud10@hotmail.com>
2025-06-21 18:06:08 +0300

    Update DeadlockAPI.py

--- DeadlockAPI.py
+++ DeadlockAPI.py
@@ -1,37 +1,41 @@
 import requests, time
+from datetime import datetime, timezone
 
 account_id = 76561198247074470
 url = f"https://api.deadlock-api.com/v1/players/{account_id}/match-history"
 
+params = {
+    "limit": 10
+}
 
-response = requests.get(url)
+response = requests.get(url, params=params)
 print(f"Response={response}") #print response code
 
-
-
-"""
 if response.status_code == 200:
-    data = response.json()
     
-    print("Response Data")
+    data = response.json()
+    totalGamesPlayed = 0
+    
+    print("Match History")
     print("-------------------------")
 
     for match in data:
+        totalGamesPlayed += 1
+        
         match_id = match.get("match_id")
-        start_time = match.get("start_time")
-        duration = match.get("duration_s")
-        game_mode = match.get("game_mode")
-        match_mode = match.get("match_mode")
-        winning_team = match.get("winning_team")
+        
+        unixTime = match.get("start_time")
+        timeStarted = datetime.fromtimestamp(unixTime, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+        
+        duration = match.get("match_duration_s")
 
         print(f"Match ID: {match_id}")
-        print(f"Start Time: {start_time}")
+        print(f"Start Time: {timeStarted}")
         print(f"Duration: {duration / 60} minutes")
-        print(f"Game Mode: {game_mode}")
-        print(f"Match Mode: {match_mode}")
-        print(f"Winning Team: {winning_team}")
+        
         print("---------------------------------------------------")
         
+    print("Games played:", str(totalGamesPlayed))
     input()
   
 else:
@@ -39,4 +43,3 @@ else:
     input()
     
 
-"""
