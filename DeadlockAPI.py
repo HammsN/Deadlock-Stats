ffc8e4366e88518b62ecbb9cc76f06835b76ab75
HammsN <fahadmahmud10@hotmail.com>
2025-06-23 12:08:00 +0300

    Update DeadlockAPI.py

--- DeadlockAPI.py
+++ DeadlockAPI.py
@@ -1,27 +1,27 @@
-import requests, time
+import requests
 from datetime import datetime, timezone
 
 # Small program to get total match history data
 
-account_id = input("Enter SteamID64: ")
-
-def validate(id):
-    if isinstance(id, int):
-        return id
+def get_steamID():
+    try:
+        account_id = int(input("Enter SteamID64: "))
+        return account_id      
+    except ValueError:
+        print("Invalid ID, try again")
+        return get_steamID()
+   
+   
+def get_response(id):
+    url = f"https://api.deadlock-api.com/v1/players/{id}/match-history"
+    response = requests.get(url)
+    
+    if response.status_code == 200: 
+        return response.json()
     else:
-        print("Invalid ID")
-        new_id = input("Enter SteamID64: ")
-        validate(new_id)
-    
-url = f"https://api.deadlock-api.com/v1/players/{account_id}/match-history"
-
-response = requests.get(url)
-
-
-if response.status_code == 200:
-    
-    data = response.json()
-    
+        raise Exception("Something wrong, server down or unidentified ID (Code:",str(response.status_code), ")")
+     
+def fetch_stats(data):
     total_games, wins, losses, abandons, denies, net_worth = 0,0,0,0,0,0
     
     print("Match History")
@@ -30,13 +30,6 @@ if response.status_code == 200:
     for match in data:
         total_games += 1
         
-        match_id = match.get("match_id")
-        
-        unixTime = match.get("start_time")
-        time_started = datetime.fromtimestamp(unixTime, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-        
-        duration = match.get("match_duration_s")
-        match_result = match.get("match_result") #Amber Hand = 0, Sapphire Flames = 1
         player_team = match.get("player_team") #0 = Loss, 1 = Victory
         denies += match.get("denies")
         net_worth += match.get("net_worth")
@@ -49,21 +42,24 @@ if response.status_code == 200:
                 wins += 1
             elif player_team == 0:
                 losses += 1
+
+    winrate = round((wins/total_games) * 100,2) if total_games else 0
     
-    winrate = float((wins/total_games) * 100) % "%.2f" if total_games else 0
-    
-    print(f"Games played: {total_games}")
-    print(f"Total Denies: {denies}")
-    print(f"Total pmoney: {net_worth}")
+    print(f"Games played: {total_games} games")
+    print(f"Total Denies: {denies} denies")
+    print(f"Total pmoney: {net_worth} souls")
     print(f"Wins: {wins}")
     print(f"Losses: {losses}")
     print(f"Abandons: {abandons}")
     print(f"Winrate: {winrate}%")
     
+    
+def main():
+    account_id = str(get_steamID())
+    history = get_response(account_id)
+    fetch_stats(history)
     input()
-  
-else:
-    print("Something wrong, server down maybe " + str(response.status_code))
-    input()
+
+main()
     
 
